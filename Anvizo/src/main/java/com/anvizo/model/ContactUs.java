0b79bd7d6e272cb4ce64a1ac88fee71a6b524a18
vaibhavjain2 <vaibhavjain_1@yahoo.com>
2020-04-23 21:45:59 +0530

    Adding hibernate.

--- Anvizo/src/main/java/com/anvizo/model/ContactUs.java
+++ Anvizo/src/main/java/com/anvizo/model/ContactUs.java
@@ -1,7 +1,21 @@
 package com.anvizo.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="ContactUs")
 public class ContactUs {
 
+	@Id
+	@Column(name = "Id")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
 	public String name;
 	
 	public String email;
