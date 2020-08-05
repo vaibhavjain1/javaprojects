8fcc2161d9d93cb68329e1ce65e65707715bac09
vaibhavjain2 <vaibhavjain_1@yahoo.com>
2020-08-05 18:32:27 +0530

    Removing old project.

--- Anvizo/src/main/java/com/anvizo/util/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.anvizo.util;
-
-public class Constants {
-
-	public static final String ANVIZO = "Anvizo";
-	public static final String DEFAULT_CONTROLLER = "/";
-	public static final String CONTACT_US_CONTROLLER = DEFAULT_CONTROLLER +"contactus";
-	public static final String SUPPORT_EMAIL = "anvizocorp@gmail.com";
-}
